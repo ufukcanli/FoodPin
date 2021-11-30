50a4171708ca276ab7ce52d578c83afb10365ccb
Ufuk Canlı <iletisim@ufukcanli.com>
2021-11-30 13:58:07 +0300

    Add custom back bar button and customize detail view

--- FoodPin/RestaurantList/RestaurantDetailView.swift
+++ FoodPin/RestaurantList/RestaurantDetailView.swift
@@ -8,10 +8,46 @@
 import SwiftUI
 
 struct RestaurantDetailView: View {
+    @Environment(\.presentationMode) var presentationMode
     var restaurant: Restaurant
     
     var body: some View {
-        Text(restaurant.name)
+        ZStack(alignment: .top) {
+            Image("restaurant")
+                .resizable()
+                .scaledToFill()
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .ignoresSafeArea()
+            
+            Color.black
+                .frame(height: 100)
+                .opacity(0.8)
+                .cornerRadius(20)
+                .padding()
+                .overlay(
+                    VStack(spacing: 5) {
+                        Text(restaurant.name)
+                        Text(restaurant.type)
+                        Text(restaurant.location)
+                    }
+                    .font(.system(.headline, design: .rounded))
+                    .foregroundColor(.white)
+                )
+        }
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button {
+                    presentationMode.wrappedValue.dismiss()
+                } label: {
+                    HStack {
+                        Image(systemName: "chevron.left")
+                        Text(restaurant.name)
+                    }
+                    .foregroundColor(.white)
+                }
+            }
+        }
     }
 }
 
