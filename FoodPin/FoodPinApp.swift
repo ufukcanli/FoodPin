74b84f4889333a19bbd58d2169cc86eae5562572
Ufuk Canlı <iletisim@ufukcanli.com>
2021-11-30 10:15:45 +0300

    Initial Commit

---
+++ FoodPin/FoodPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPinApp.swift
+//  FoodPin
+//
+//  Created by Ufuk Canlı on 30.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct FoodPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
