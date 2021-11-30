670be1ae10e9674ee77c1f79c718de6471dbd1e5
Ufuk Canlı <iletisim@ufukcanli.com>
2021-11-30 13:25:31 +0300

    Fix some bugs and add navigation view

--- FoodPin/RestaurantList/RestaurantListView.swift
+++ FoodPin/RestaurantList/RestaurantListView.swift
@@ -11,84 +11,87 @@ struct RestaurantListView: View {
     @StateObject private var viewModel = RestaurantListViewModel()
     
     var body: some View {
-        List {
-            ForEach(viewModel.restaurants) { restaurant in
-                BasicTextImageRow(
-                    name: restaurant.name,
-                    type: restaurant.type,
-                    location: restaurant.location,
-                    isFavorite: restaurant.isFavorite
-                )
-                // TODO: Swipe actions iOS15
-                .contextMenu {
-                    Button {
-                        viewModel.showError.toggle()
-                    } label: {
-                        HStack {
-                            Text("Reserve a table")
-                            Image(systemName: "phone")
-                        }
-                    }
-                    
-                    Button {
-                        viewModel.currentRestaurant = restaurant
-                        viewModel.markFavorite()
-                    } label: {
-                        HStack {
-                            Text(restaurant.isFavorite ? "Remove from favorites" : "Mark as favorite")
-                            Image(systemName: restaurant.isFavorite ? "heart.fill" : "heart")
-                        }
-                    }
-                    
-                    Button {
-                        viewModel.currentRestaurant = restaurant
-                        viewModel.showShare.toggle()
-                    } label: {
-                        HStack {
-                            Text("Share this restaurant")
-                            Image(systemName: "square.and.arrow.up")
-                        }
-                    }
-                }
-                .onTapGesture {
-                    viewModel.showOptions.toggle()
-                    viewModel.currentRestaurant = restaurant
-                }
-                .actionSheet(isPresented: $viewModel.showOptions) {
-                    ActionSheet(
-                        title: Text("What do you want to do?"),
-                        message: nil,
-                        buttons: [
-                            .default(Text("Reserve a table")) {
-                                viewModel.showError.toggle()
-                            },
-                            .default(Text(restaurant.isFavorite ? "Remove from favorites" : "Mark as favorite")) {
-                                viewModel.markFavorite()
-                            },
-                            .cancel()
-                        ]
+        NavigationView {
+            List {
+                ForEach(viewModel.restaurants) { restaurant in
+                    BasicTextImageRow(
+                        name: restaurant.name,
+                        type: restaurant.type,
+                        location: restaurant.location,
+                        isFavorite: restaurant.isFavorite
                     )
-                }
-                .alert(isPresented: $viewModel.showError) {
-                    Alert(
-                        title: Text("Not yet available"),
-                        message: Text("Sorry, this feature is not available yet. Please try again later."),
-                        primaryButton: .default(Text("OK")),
-                        secondaryButton: .cancel()
-                    )
-                }
-                .sheet(isPresented: $viewModel.showShare) {
-                    let defaultText = "Just checking in at \(restaurant.name)"
-                    if let imageToShare = UIImage(named: restaurant.image) {
-                        ActivityView(activityItems: [defaultText, imageToShare])
-                    } else {
-                        ActivityView(activityItems: [defaultText])
+                    // TODO: Swipe actions iOS15
+                    .contextMenu {
+                        Button {
+                            viewModel.showError.toggle()
+                        } label: {
+                            HStack {
+                                Text("Reserve a table")
+                                Image(systemName: "phone")
+                            }
+                        }
+                        
+                        Button {
+                            viewModel.currentRestaurant = restaurant
+                            viewModel.markFavorite()
+                        } label: {
+                            HStack {
+                                Text(viewModel.currentRestaurant?.isFavorite == true ? "Remove from favorites" : "Mark as favorite")
+                                Image(systemName: viewModel.currentRestaurant?.isFavorite == true ? "heart.fill" : "heart")
+                            }
+                        }
+                        
+                        Button {
+                            viewModel.currentRestaurant = restaurant
+                            viewModel.showShare.toggle()
+                        } label: {
+                            HStack {
+                                Text("Share this restaurant")
+                                Image(systemName: "square.and.arrow.up")
+                            }
+                        }
                     }
+                    .onTapGesture {
+                        viewModel.showOptions.toggle()
+                        viewModel.currentRestaurant = restaurant
+                    }
+                    .actionSheet(isPresented: $viewModel.showOptions) {
+                        ActionSheet(
+                            title: Text("What do you want to do?"),
+                            message: nil,
+                            buttons: [
+                                .default(Text("Reserve a table")) {
+                                    viewModel.showError.toggle()
+                                },
+                                .default(Text(viewModel.currentRestaurant?.isFavorite == true ? "Remove from favorites" : "Mark as favorite")) {
+                                    viewModel.markFavorite()
+                                },
+                                .cancel()
+                            ]
+                        )
+                    }
+                    .alert(isPresented: $viewModel.showError) {
+                        Alert(
+                            title: Text("Not yet available"),
+                            message: Text("Sorry, this feature is not available yet. Please try again later."),
+                            primaryButton: .default(Text("OK")),
+                            secondaryButton: .cancel()
+                        )
+                    }
+                    .sheet(isPresented: $viewModel.showShare) {
+                        let defaultText = "Just checking in at \(restaurant.name)"
+                        if let imageToShare = UIImage(named: restaurant.image) {
+                            ActivityView(activityItems: [defaultText, imageToShare])
+                        } else {
+                            ActivityView(activityItems: [defaultText])
+                        }
+                    }
+                }
+                .onDelete { indexSet in
+                    viewModel.restaurants.remove(atOffsets: indexSet)
                 }
             }
-            .onDelete { indexSet in
-                viewModel.restaurants.remove(atOffsets: indexSet)
-            }
+            .navigationTitle("FoodPin")
         }
     }
 }
